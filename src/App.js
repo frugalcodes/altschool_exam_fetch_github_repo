13d5ef99a89fc2e02446da37e3bf4d0c1715a7ac
Frugalcodes <samueligwe63@gmail.com>
2022-11-06 23:58:02 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route path='/' exact element={<Home />} />
       
         <Route path='/:id' exact element={<Results />}/>
-         <Route path='*' element={<Errornow/>}/>
+       
         </Routes>
         </ErrorBoundary>
     </div>
